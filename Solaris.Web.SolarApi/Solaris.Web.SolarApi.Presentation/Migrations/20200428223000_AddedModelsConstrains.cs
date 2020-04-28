4b82d5800b457e93149394d7f6a89bf89272990b
Filip <gagiu.filip@gmail.com>
2020-04-29 01:36:14 +0300

    SLRAPI-7 Fixed Models Propriety Types

---
+++ Solaris.Web.SolarApi/Solaris.Web.SolarApi.Presentation/Migrations/20200428223000_AddedModelsConstrains.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Solaris.Web.SolarApi.Presentation.Migrations
+{
+    public partial class AddedModelsConstrains : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                "Name",
+                "SolarSystems",
+                "varchar(256)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET latin1",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                "Name",
+                "Planets",
+                "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET latin1",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                "Description",
+                "Planets",
+                "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET latin1",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                "Name",
+                "SolarSystems",
+                "longtext CHARACTER SET latin1",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)");
+
+            migrationBuilder.AlterColumn<string>(
+                "Name",
+                "Planets",
+                "longtext CHARACTER SET latin1",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+
+            migrationBuilder.AlterColumn<string>(
+                "Description",
+                "Planets",
+                "longtext CHARACTER SET latin1",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Solaris.Web.SolarApi/Solaris.Web.SolarApi.Presentation/Migrations/20200428223508_SolarSystemSpacePositionToVarchar.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Solaris.Web.SolarApi.Presentation.Migrations
+{
+    public partial class SolarSystemSpacePositionToVarchar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                "SpacePosition",
+                "SolarSystems",
+                "varchar(256)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET latin1");
+
+            migrationBuilder.AlterColumn<long>(
+                "DistanceToEarth",
+                "SolarSystems",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                "SpacePosition",
+                "SolarSystems",
+                "longtext CHARACTER SET latin1",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)");
+
+            migrationBuilder.AlterColumn<string>(
+                "DistanceToEarth",
+                "SolarSystems",
+                "varchar(256)",
+                nullable: false,
+                oldClrType: typeof(long));
+        }
+    }
+}

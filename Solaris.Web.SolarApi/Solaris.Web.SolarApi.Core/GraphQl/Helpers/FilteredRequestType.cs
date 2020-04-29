36c03267b9f6bd78546e96af3d469a4332033cf4
Filip <gagiu.filip@gmail.com>
2020-04-29 23:50:25 +0300

    SLRAPI-8 Small Fix

--- Solaris.Web.SolarApi/Solaris.Web.SolarApi.Core/GraphQl/Helpers/FilteredRequestType.cs
+++ Solaris.Web.SolarApi/Solaris.Web.SolarApi.Core/GraphQl/Helpers/FilteredRequestType.cs
@@ -1,16 +1,15 @@
 using GraphQL.Types;
-using Solaris.Web.SolarApi.Core.Models.Helpers;
 using Solaris.Web.SolarApi.Core.Models.Interfaces;
 
 namespace Solaris.Web.SolarApi.Core.GraphQl.Helpers
 {
     public class FilteredRequestType<T> : InputObjectGraphType<IFilter<T>> where T : IIdentifier
     {
-        public new const string Description = "";
+        public new const string Description = "Filter used to refine the search response";
 
         public FilteredRequestType()
         {
-            Field(x => x.SearchTerm, true);
+            Field(x => x.SearchTerm, true).Description($"The search term which will be used to filter the list of {typeof(T).Name}s");
         }
     }
 }
